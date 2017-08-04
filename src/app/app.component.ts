fd22c12c7a14d8bc6ea87308d33c99eefc7d61b1
Artem Koroteev <artem_koroteev@epam.com>
2017-08-05 00:02:33 +0400

    Change site template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
+﻿import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
     title = 'app';
-
-    constructor(private router: Router) { }
-
-    ngOnInit() {
-        this.router.events.subscribe((evt) => {
-            if (!(evt instanceof NavigationEnd)) {
-                return;
-            }
-            window.scrollTo(0, 0)
-        });
-    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,43 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
+﻿import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NavigationComponent } from './navigation/navigation.component';
-import { IntroHeaderComponent } from './intro-header/intro-header.component';
-import { AboutComponent } from './about/about.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
-import { ContactComponent } from './contact/contact.component';
 import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent,
-    IntroHeaderComponent,
-    AboutComponent,
     PortfolioComponent,
-    ContactComponent,
-    FooterComponent
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
-      BrowserModule,
-      RouterModule.forRoot([
-          {
-              path: 'home',
-              component: AboutComponent
-          },
-          {
-              path: 'contact',
-              component: ContactComponent
-          },
-          {
-              path: '',
-              redirectTo: '/home',
-              pathMatch: 'full'
-          }
-      ])
-  ],
+      BrowserModule
+      ],
   providers: [],
   bootstrap: [AppComponent]
 })
