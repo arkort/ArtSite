a81ecb694f2dfc107314acb8e3b9c97a85b44bc8
Artem Koroteev <artem_koroteev@epam.com>
2017-08-05 12:11:47 +0400

    Add simple form without logic

---
+++ src/app/admin/admin.component.ts
@@ -0,0 +1,14 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin',
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.css']
+})
+export class AdminComponent{
+  user:Object={};
+
+  login(){
+    console.log(this.user);
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,41 @@
 ﻿import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PortfolioComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    AdminComponent
   ],
   imports: [
-      BrowserModule
-      ],
+      BrowserModule,
+      FormsModule,
+      RouterModule.forRoot([
+          {
+              path: 'home',
+              component: HeaderComponent
+          },
+          {
+              path: 'admin',
+              component: AdminComponent
+          },
+          {
+              path: '',
+              redirectTo: '/home',
+              pathMatch: 'full'
+          }
+      ])
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
