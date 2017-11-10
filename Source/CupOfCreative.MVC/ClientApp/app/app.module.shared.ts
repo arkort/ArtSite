a2f4c254a25dd6fe998d32d5828e93a502e3b5f0
Artem <tumofeu@gmail.com>
2017-11-10 17:51:11 +0400

    Add routing and admin component

--- Source/CupOfCreative.MVC/ClientApp/app/app.module.shared.ts
+++ Source/CupOfCreative.MVC/ClientApp/app/app.module.shared.ts
@@ -2,11 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 
+import { Routes, RouterModule } from '@angular/router';
+
 import { AppComponent } from './components/app/app.component';
 import { PortfolioComponent } from './components/portfolio/portfolio.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
+import { AdminComponent } from './components/admin/admin.component';
+
 import { FilterPipe } from './FilterPipe';
+
+const appRoutes: Routes = [
+    { path: '', component: PortfolioComponent },
+    { path: 'admin', component: AdminComponent },
+    { path: '**', component: PortfolioComponent }
+];
 
 @NgModule({
     declarations: [
@@ -14,11 +24,13 @@ import { FilterPipe } from './FilterPipe';
         PortfolioComponent,
         FooterComponent,
         HeaderComponent,
+        AdminComponent,
         FilterPipe
     ],
     imports: [
         BrowserModule,
-        HttpModule
+        HttpModule,
+        RouterModule.forRoot(appRoutes)
     ],
     providers: [HttpModule],
     bootstrap: [AppComponent]
